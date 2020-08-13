d4ba621b536d454f7d278e0f256b781617582046
matte54 <king_matthew@hotmail.com>
2020-08-13 11:55:50 +0200

    stuff

--- spider_silk.py
+++ spider_silk.py
@@ -8,7 +8,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('Arachne')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///var/www/arachne/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/www/arachne/app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
