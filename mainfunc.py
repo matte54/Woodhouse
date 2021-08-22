567b31633a943f2b78950dc2581f88edd050870c
matte54 <king_matthew@hotmail.com>
2021-08-22 08:53:13 +0200

    x

--- mainfunc.py
+++ mainfunc.py
@@ -57,9 +57,9 @@ def get_speech(client, trigger):
     print(emojis)#debug
     #emoji_names = [emoji.name for emoji in client.emojis]
     emoji_names = []
-    emoji.name for emoji in client.emojis:
+    for emoji in client.emojis:
         if emoji.animated == False:
-            emoji_names.append(emoji_name) #testing new
+            emoji.name.append(emoji_name) #testing new
     #add 10 emoji choices
     sEmo = random.choices(emoji_names, k=10)
     sEmo = [':%s:' % emoji_name for emoji_name in sEmo]
